4415c32189317e560d8b9e00de34f4498cece825
Александра Морозова <realsanya@MacBook-Air-Aleksandra.local>
2021-03-30 00:53:29 +0300

    add module

---
+++ trello-web/src/main/java/ru/itis/javalab/trello/web/TrelloWebApplication.java
@@ -0,0 +1,13 @@
+package ru.itis.javalab.trello.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrelloWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrelloWebApplication.class, args);
+    }
+
+}
