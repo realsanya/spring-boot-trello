4b6346590ea2389845ddfc32342ff40f1bc5d6f5
realsanya <sashamorozova01@mail.ru>
2021-05-03 13:24:10 +0300

    config from aspects

--- trello-impl/src/main/java/ru/itis/javalab/trello/impl/config/TrelloImplConfiguration.java
+++ trello-impl/src/main/java/ru/itis/javalab/trello/impl/config/TrelloImplConfiguration.java
@@ -5,16 +5,18 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
-@ComponentScan("ru.itis.javalab.trello.impl.services")
+@ComponentScan({"ru.itis.javalab.trello.impl.services", "ru.itis.javalab.trello.impl.aspect"})
 @EnableJpaRepositories(basePackages = "ru.itis.javalab.trello.impl.repositories")
 @EnableTransactionManagement
 @EntityScan(basePackages = "ru.itis.javalab.trello.impl.models")
+@EnableAspectJAutoProxy
 public class TrelloImplConfiguration {
     @Bean
     public ModelMapper modelMapper() {
